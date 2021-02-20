e842d3c7448cb2dd0f7c7022bad7a1224cd78b5e
hexianhao <495781680@qq.com>
2021-02-20 17:32:43 +0800

    finishing lab7

--- lock_server_cache_rsm.h
+++ lock_server_cache_rsm.h
@@ -2,16 +2,38 @@
 #define lock_server_cache_rsm_h
 
 #include <string>
+#include <map>
+#include <set>
+#include <mutex>
 
 #include "lock_protocol.h"
 #include "rpc.h"
 #include "rsm_state_transfer.h"
 #include "rsm.h"
+#include "fifo.h"
 
 class lock_server_cache_rsm : public rsm_state_transfer {
  private:
   int nacquire;
   class rsm *rsm;
+  
+  enum lock_state {
+    FREE,
+    LOCKED,
+    // 为了防止出现deadlock，需要以下两个状态
+    LOCKED_AND_WAIT,  // 表示有其它客户端等待锁
+    RETRYING          // 表示服务器提醒客户端重试
+  };
+
+  struct lock_entry {
+      std::string owner;
+      std::set<std::string> waitSet;
+      lock_state state;
+  };
+  
+  std::map<lock_protocol::lockid_t, lock_entry> lockMap;
+  std::mutex mtx;
+
  public:
   lock_server_cache_rsm(class rsm *rsm = 0);
   lock_protocol::status stat(lock_protocol::lockid_t, int &);
